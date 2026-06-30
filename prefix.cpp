68629201abe32f6c5cc37fd067a5f6601095b7ad
muskan13604 <muskanyadav02420@gmail.com>
2026-06-30 14:48:14 +0530

    next

---
+++ prefix.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main(){
+    int a;
+    cin>>a;
+    int sum=0;
+    int arr[a];
+    for(int i=0; i<a; i++){
+        cin>>arr[i];
+        sum+=arr[i];
+    }
+    cout<<sum;
+
+}
