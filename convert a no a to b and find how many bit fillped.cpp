5b4c319effeecd1e7789ef08c909f9b74ce6c264
muskan13604 <muskanyadav02420@gmail.com>
2026-07-01 16:44:00 +0530

    code

---
+++ convert a no a to b and find how many bit fillped.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int countBitsFlip(int a, int b) {
+        int x = a ^ b;   
+        int count = 0;
+
+        while (x > 0) {
+            if (x & 1)   
+                count++;
+
+            x = x >> 1;  
+        }
+
+        return count;
+    }
+};
