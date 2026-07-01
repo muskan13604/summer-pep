4502870f866f4216223b98f1473e9a2041642265
muskan13604 <muskanyadav02420@gmail.com>
2026-07-01 15:42:14 +0530

    code

---
+++ swapping of 2 number using XOR.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+
+    cout << "Before swapping: a = " << a << ", b = " << b << endl;
+
+    // Swapping using XOR
+ y
+    cout << "After swapping: a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
