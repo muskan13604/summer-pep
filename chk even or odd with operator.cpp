74277898957b87eb67f457d930d827d042856faa
muskan13604 <muskanyadav02420@gmail.com>
2026-07-01 14:59:39 +0530

    code

---
+++ chk even or odd with operator.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+    cin>>n;
+    
+    if(n & 1)
+        cout<<"Odd";
+    else
+        cout<<"Even";
+        
+    return 0;
+
+}
