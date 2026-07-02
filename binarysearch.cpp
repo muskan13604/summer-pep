8513f52a96ed900cec742fac5630bc5f9c84fa3c
muskan13604 <muskanyadav02420@gmail.com>
2026-07-02 16:12:28 +0530

    code

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -1,29 +1,29 @@
 #include <iostream>
 using namespace std;
 
-int binarySearch(int arr[], int n, int target) {
-    int low = 0, high = n - 1;
+int binarySearch(int arr[], int l, int h, int t) {
 
-    while (low <= high) {
-        int mid = low + (high - low) / 2;
+    // Base Case
+    if (l > h)
+        return -1;
 
-        if (arr[mid] == target)
-            return mid;
+    int mid = l + (h - l) / 2;
 
-        else if (target > arr[mid])
-            high = mid - 1;
+    if (arr[mid] == t)
+        return mid;
 
-        else
-            low = mid + 1;    
-    }
 
-    return -1;
+    if (t > arr[mid])
+        return binarySearch(arr, l, mid - 1, t);
+
+    return binarySearch(arr, mid + 1, h, t);
 }
 
 int main() {
     int arr[] = {25, 20, 18, 12, 11, 9, 8, 7, 5, 3, 2, 1};
     int n = sizeof(arr) / sizeof(arr[0]);
+
     int target = 20;
 
-    cout << binarySearch(arr, n, target);
+    cout << binarySearch(arr, 0, n - 1, target);
 }
