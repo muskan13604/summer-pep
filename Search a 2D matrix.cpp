3d8833214ee857955f44e8f9e4c51f408c617230
muskan13604 <muskanyadav02420@gmail.com>
2026-07-02 16:39:50 +0530

    code

--- Search a 2D matrix.cpp
+++ Search a 2D matrix.cpp
@@ -7,20 +7,50 @@ public:
         int n = matrix[0].size();
 
         int row = 0;
-        int col = n - 1;   // Start from top-right
+        int col = n - 1;   
 
         while (row < m && col >= 0) {
             if (matrix[row][col] == target) {
                 return true;
             }
             else if (matrix[row][col] > target) {
-                col--;      // Move left
+                col--;     
             }
             else {
-                row++;      // Move down
+                row++;      
             }
         }
 
         return false;
+    }
+};
+
+
+//recursively
+class Solution {
+public:
+    bool solve(vector<vector<int>>& matrix, int row, int col, int target) {
+        int m = matrix.size();
+
+
+        if (row >= m || col < 0)
+            return false;
+
+
+        if (matrix[row][col] == target)
+            return true;
+
+        
+        if (matrix[row][col] > target)
+            return solve(matrix, row, col - 1, target);
+
+        
+        return solve(matrix, row + 1, col, target);
+    }
+
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int n = matrix[0].size();
+
+        return solve(matrix, 0, n - 1, target);
     }
 };
