c6a86ef1b8e855cecbc659bb71157e7fa4ef42b3
muskan13604 <muskanyadav02420@gmail.com>
2026-07-08 15:18:52 +0530

    code

--- leetcode 98.cpp
+++ leetcode 98.cpp
@@ -21,3 +21,39 @@ public:
         return check(root, LLONG_MIN, LLONG_MAX);
     }
 };
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void validBST(TreeNode* root, TreeNode* &prev, bool &ans){
+        if(root == NULL){
+            return;
+        }
+        validBST(root->left,prev,ans);
+        if(prev!= NULL && prev->val>= root->val){
+            ans=false;
+            return;
+        }
+        prev = root;
+         validBST(root->right,prev,ans);
+    }
+    bool isValidBST(TreeNode* root) {
+        TreeNode* prev = NULL;
+        bool ans = true;
+        validBST(root,prev,ans);
+        return ans;
+    }
+};
